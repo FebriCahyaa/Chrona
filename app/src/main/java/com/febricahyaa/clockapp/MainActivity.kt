b605c1a0e6386df684027264959988f5facc29cc
FebriCahyaa <febricahya12345@gmail.com>
2026-09-15 01:47:05 +0700

    fix: opt in to experimental Material3 API

--- app/src/main/java/com/febricahyaa/clockapp/MainActivity.kt
+++ app/src/main/java/com/febricahyaa/clockapp/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.febricahyaa.clockapp
 
 import android.os.Bundle
@@ -48,6 +50,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
