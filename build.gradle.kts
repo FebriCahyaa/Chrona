3be93b2328a69fe12449331bbf17685817f29d3c
FEBRIAN RAHMAD CAHYA <febricahya12345@gmail.com>
2026-09-17 19:50:10 +0000

    fix(build): restore valid root Gradle script structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,12 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
     }
 }
-                }
-            }
-        }
-
 
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }
+
 // CHRONA-SECURITY-RESOLUTION
 // These constraints apply to project configurations so patched transitive
 // versions are selected when vulnerable versions are requested.
@@ -36,4 +33,3 @@ allprojects {
         }
     }
 }
-
